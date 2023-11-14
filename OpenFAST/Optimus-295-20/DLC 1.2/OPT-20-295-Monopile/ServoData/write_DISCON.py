29a325a15fa4a82f465667ed3c81b20d82e37fdb
SimonWeich <simon.weich.2000@t-online.de>
2023-11-14 22:59:42 +0100

    Updated ServoData / Added ROSCO Toolbox
    Added .py for generating a cP-surface
    Added .py for generating a DISON.IN
    Added ROSCO-Toolbox as py-lib

---
+++ OpenFAST/Optimus-295-20/DLC 1.2/OPT-20-295-Monopile/ServoData/write_DISCON.py
@@ -0,0 +1,36 @@
+# Python modules
+import os
+# ROSCO toolbox modules
+from ROSCO_toolbox import controller as ROSCO_controller
+from ROSCO_toolbox import turbine as ROSCO_turbine
+from ROSCO_toolbox.utilities1 import write_DISCON
+from ROSCO_toolbox.inputs.validation import load_rosco_yaml
+
+
+tune_dir = os.getcwd()
+parameter_filename = 'OPT_20_295.yaml'
+inps = load_rosco_yaml(parameter_filename)
+path_params         = inps['path_params']
+turbine_params      = inps['turbine_params']
+controller_params   = inps['controller_params']
+
+turbine         = ROSCO_turbine.Turbine(turbine_params)
+controller      = ROSCO_controller.Controller(controller_params)
+
+FAST_InputFile = '../OPT-20-295.fst'
+
+cp_filename = os.path.join(tune_dir, path_params['FAST_directory'], path_params['rotor_performance_filename'])
+
+turbine.load_from_fast(
+    FAST_InputFile,
+    os.path.join(tune_dir,path_params['FAST_directory']),
+    rot_source='txt',txt_filename= cp_filename
+    )
+
+controller.tune_controller(turbine)
+
+param_file = r'C:\Users\MightyH4wk\Documents\GitHub\OPTIMUS_ROSCO\PC_Test\DISCON.IN'
+write_DISCON(turbine,controller,
+param_file=param_file,
+txt_filename=cp_filename
+)
---
+++ OpenFAST/Optimus-295-20/DLC 1.2/OPT-20-295-Monopile/ServoData/write_cPsurface.py
@@ -0,0 +1,32 @@
+
+# Python modules
+import os 
+# ROSCO toolbox modules 
+from ROSCO_toolbox import turbine as ROSCO_turbine
+from ROSCO_toolbox.utilities1 import write_rotor_performance
+from ROSCO_toolbox.inputs.validation import load_rosco_yaml
+
+# Initialize parameter dictionaries
+turbine_params = {}
+control_params = {}
+
+# Load yaml file
+this_dir = os.path.dirname(os.path.abspath(__file__))
+tune_dir = os.getcwd()
+parameter_filename = 'OPT_20_295.yaml'
+inps = load_rosco_yaml(parameter_filename)
+path_params         = inps['path_params']
+turbine_params      = inps['turbine_params']
+controller_params   = inps['controller_params']
+
+# Load turbine data from openfast model
+turbine = ROSCO_turbine.Turbine(turbine_params)
+turbine.load_from_fast(
+    path_params['FAST_InputFile'],
+    os.path.join(this_dir, path_params['FAST_directory']),
+    rot_source='cc-blade',
+    txt_filename=None)
+
+# Write rotor performance text file
+txt_filename = os.path.join(this_dir,'Cp_Ct_Cq_OPTIMUS_20_295.txt')
+write_rotor_performance(turbine, txt_filename=txt_filename)
